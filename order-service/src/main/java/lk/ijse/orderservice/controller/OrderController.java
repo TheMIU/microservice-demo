dc6aaf066882a633b81b6f01ab25811d9154cff5
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2023-11-25 22:30:58 +0530

    Communicate between services

--- order-service/src/main/java/lk/ijse/orderservice/controller/OrderController.java
+++ order-service/src/main/java/lk/ijse/orderservice/controller/OrderController.java
@@ -1,8 +1,10 @@
 package lk.ijse.orderservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/order")
@@ -10,5 +12,18 @@ public class OrderController {
     @GetMapping
     public String get(){
         return "Orders get method invoked";
+    }
+
+    ///////////////////////////////////
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("to-customer")
+    public String test() {
+        return restTemplate.getForObject("http://localhost:8081/app/customer", String.class);
+
+        // post request ,,,
+        // return restTemplate.postForObject("http://localhost:8082/app/order/save", User, ResponseUtil);
     }
 }
