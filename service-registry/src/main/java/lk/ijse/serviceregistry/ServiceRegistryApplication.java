c0c239531fc1682ff8e1183e3af0b7ca1d910ff0
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2023-12-09 12:36:26 +0530

    api gateway added

--- service-registry/src/main/java/lk/ijse/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/lk/ijse/serviceregistry/ServiceRegistryApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class ServiceRegistryApplication {
 
     public static void main(String[] args) {
