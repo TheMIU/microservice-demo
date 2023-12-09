fa58c3c2199d784503dcda608908cde8e6f54f96
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2023-12-09 11:11:59 +0530

    Update ServiceRegistryApplication.java

--- service-registry/src/main/java/lk/ijse/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/lk/ijse/serviceregistry/ServiceRegistryApplication.java
@@ -2,10 +2,11 @@ package lk.ijse.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ServiceRegistryApplication {
 
     public static void main(String[] args) {
